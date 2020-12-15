527f922b608525b4db14cd35b32f0c0b50c36632
Kelly Bruyninx <kelzoe@skynet.be>
2020-12-15 18:37:28 +0100

    bouton recent

--- script.js
+++ script.js
@@ -5,12 +5,11 @@ const app = document.getElementById('app');
 // affichage des poster de films sur la page:
 let moviesContainer = '<section class="containerFilms">';
 moviesContainer += '<h1> "Films on the theme of "hacking"</h1>';
-moviesContainer += '<button> Recent Film Only </button> </br>';
+moviesContainer += '<button id="recent" class="recent"> View Recent Film Only </button> </br>';
 for (let i = 0; i < movies.length; i++) {
   if (movies[i].img) {
     moviesContainer += `  
-    <img src='posters/${movies[i].imdb}.jpg' class="img"  id="${movies[i].imdb}" alt="afficheFilmHacking">
-   `;
+    <img src='posters/${movies[i].imdb}.jpg' class="img"  id="${movies[i].imdb}" alt="afficheFilmHacking"> `;
   } else {
     moviesContainer += `<img class="img imgColorer"  id="${movies[i].imdb}">`;
     // moviesContainer += `<div class="img imgColorer divImg"  id="${movies[i].imdb}">
@@ -23,7 +22,6 @@ moviesContainer += '</section>';
 document.body.addEventListener('click', (e) => {
   if (e.target.matches('.img')) {
     // console.log(e.target);
-
     let popUpInfosFilm = '';
     movies.forEach((element) => {
       if (e.target.id === element.imdb) {
@@ -49,62 +47,93 @@ document.body.addEventListener('click', (e) => {
 // délégation d'évenement : on click sur les icone croix pour fermer la popUp
 document.body.addEventListener('click', (el) => {
   if (el.target.matches('.fa-times-circle')) {
-    // popUpInfosFilm.remove();
     const popupInfoToRemove = document.querySelector('.fondPopUp');
     popupInfoToRemove.remove();
   }
 });
 app.innerHTML += moviesContainer;
 
-/* // délégation d'évenement : on click sur les icone croix pour fermer la popUp
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('.fa-times-circle')) {
-    popUpInfosFilm.remove();
+// Bouton "Recent film only" qui cache  les films réalisés avant l'an 2000
+/*
+document.body.addEventListener('click', (elem) => {
+  if (elem.target.matches('.recent')) {
+    console.log(elem.target); // = ok
+    const filmsRecents = app.querySelectorAll('.img');
+    // console.log(filmsRecents);
+    filmsRecents.forEach((filmRecent, i) => {
+      if (movies[i].year < 2000) {
+        filmRecent.classList.add('d-none');
+      }
+    });
   }
 }); */
 
-// ------------------ Ancien code test ---------------------
-/* Ancienne version mais plus lisible de faire avec un variable vide au depart
-function popUpInfos() {
-  let popUpInfosFilm = '<div id="popUpInfos" class="popUp">';
-  for (let j = 0; j < movies.length; j++) {
-    popUpInfosFilm += `
-    <i class="far fa-times-circle"></i>
-    <h3> Title: ${movies[j].title}</h3>
-    <p> Genres: </br> ${movies[j].genres} </p>
-    <p> Year: ${movies[j].year}</p>
-    <p> Note: ${movies[j].note}</p>
-    <p> Plot: </br> ${movies[j].plot} </p>
-  `;
-    popUpInfosFilm = '</div>';
-  }
-} */
+// si reclic remet all :
+// test 1: avec vérification target
+document.body.addEventListener('click', (elem) => {
+  if (elem.target.matches('.recent')) {
+    console.log(elem.target); // = ok
 
-/* premier test function:
- function popUpInfos() {
-  let popUpInfosFilm = '';
-  for (let j = 0; j < movies.length; j++) {
-    if (movies[j].img) {
-      popUpInfosFilm += `
-    <div id="popUpInfos" class="popUp">
-      <i class="far fa-times-circle"></i>
-      <h3> Title: ${movies[j].title}</h3>
-      <p> Genres: </br> ${movies[j].genres} </p>
-      <p> Year: ${movies[j].year}</p>
-      <p> Note: ${movies[j].note}</p>
-      <p> Plot: </br> ${movies[j].plot} </p>
-     </div>
-  `;
-      console.log(popUpInfosFilm);
-      app.innerHTML += popUpInfosFilm;
+    const filmsRecents = app.querySelectorAll('.img');
+    // console.log(filmsRecents);
+
+    filmsRecents.forEach((filmRecent, i) => {
+      if (movies[i].year < 2000) {
+        if (filmRecent.classList.contains('d-none')) {
+          filmRecent.classList.remove('d-none');
+        } else {
+          filmRecent.classList.add('d-none');
+        }
+      }
+    });
+  }
+});
+// --- version ou le nom bouton change nom = problème avec la popup--------
+/*
+// test 2 : changement du boutton au click
+const buttonRecent = document.getElementById('recent');
+// console.log(buttonRecent);
+buttonRecent.addEventListener('click', (elem) => {
+  if (elem.target.matches('.recent')) {
+    // console.log(elem.target);
+
+    const filmsRecents = app.querySelectorAll('.img');
+    // console.log(filmsRecents);
+
+    filmsRecents.forEach((filmRecent, i) => {
+      if (movies[i].year < 2000) {
+        if (filmRecent.classList.contains('d-none')) {
+          filmRecent.classList.remove('d-none');
+          buttonRecent.innerHTML = 'View Recent films only';
+        } else {
+          filmRecent.classList.add('d-none');
+          buttonRecent.innerHTML = 'View All films';
+        }
+      }
+    });
+  }
+}); */
+
+/*
+// test 3 : sans vérification target = inutile de verifier car on passe par getElementById
+
+const buttonRecent = document.getElementById('recent');
+// console.log(buttonRecent);
+buttonRecent.addEventListener('click', () => {
+  // console.log('c est le boutton!');
+  const filmsRecents = app.querySelectorAll('.img');
+  // console.log(filmsRecents);
+
+  filmsRecents.forEach((filmRecent, i) => {
+    if (movies[i].year > 2000) {
+      if (filmRecent.classList.contains('d-none')) {
+        filmRecent.classList.remove('d-none');
+        buttonRecent.innerHTML = 'View Recent films only';
+      } else {
+        filmRecent.classList.add('d-none');
+        buttonRecent.innerHTML = 'View All films';
+      }
     }
-  }
-}
-// délégation d'évenement : on click sur les img de films = affiche popUp d'infos
-document.body.addEventListener('click', (e) => {
-  if (e.target.matches('.img')) {
-    console.log(e.target);
-    popUpInfos();
-  }
+  });
 });
 */
